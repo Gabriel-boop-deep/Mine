45f45c5a2c449404f28d5a3eed5cd29beda57724
Gabriel-boop-deep <72049404+Gabriel-boop-deep@users.noreply.github.com>
2022-01-26 21:50:21 -0300

    Update minecraft.cs

--- minecraft.cs
+++ minecraft.cs
@@ -7,11 +7,11 @@
 
  class Program {
    static void Main (string[] args) {
-   Personagem person1= new Personagem("ExemploA","Ficcao",1998,0);
-   Personagem person2 = new Personagem("ExemploB","Romance",2018,0);
-   Personagem person3 = new Personagem("ExemploC","Ficcao",2018,0);
-   Personagem person4 = new Personagem("ExemploD","Academico",2017,0);
-   Personagem person5 = new Personagem("ExemploE","Ficcao",1998,0);
+   Personagem person1= new Personagem("Bob","Masculino",1998,0);
+   Personagem person2 = new Personagem("Steve","Masculino",2018,0);
+   Personagem person3 = new Personagem("Charl","Feminino",2018,0);
+   Personagem person4 = new Personagem("D","Feminino",2017,0);
+   Personagem person5 = new Personagem("Elloy","Masculino",1998,0);
     
     
     Console.WriteLine(person1);
